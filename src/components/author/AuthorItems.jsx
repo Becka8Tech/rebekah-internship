82bf603af8f67771ca5688424342d08bed33a92e
Becka8Tech <rebekahmiller18@gmail.com>
2025-04-20 19:50:20 -0600

    Save Current Branch

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,62 +1,79 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton.jsx";
 
 const AuthorItems = () => {
+  const { id } = useParams(); // Use the id from params
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const response = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`); // Use id instead of authorId
+        const resultData = response.data;
+
+        if (Array.isArray(resultData) && resultData.length > 0) {
+          setData(resultData);
+        } else {
+          console.error("Error: No valid data returned.");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
-        <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
+        {loading ? (
+          <div className="row">
+            {new Array(8).fill(0).map((_, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="nft__item">
+                  <div className="author_list_pp">
+                    <Link to="">
+                      <Skeleton width="100%" height="100%" borderRadius="10px" />
+                    </Link>
                   </div>
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>97</span>
+                  <div className="nft__item_wrap">
+                    <Skeleton width="100%" height="260px" borderRadius="10px" />
+                    <Skeleton width="50px" height="50px" borderRadius="50%" />
+                    <Skeleton width="70%" height="24px" borderRadius="5px" />
+                    <Skeleton width="40%" height="18px" borderRadius="5px" />
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : data.length > 0 ? (
+          <div className="row">
+            {data.map((nftCollection) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={nftCollection.id}>
+                <Link to="/item-details">
+                  <div className="nft__item_info">
+                    <img src={nftCollection.nftImage} className="lazy nft__item_preview" alt="" />
+                    <h4>{nftCollection.title}</h4>
+                    <div className="nft__item_price">{nftCollection.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{nftCollection.likes}</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>No data available</div>
+        )}
       </div>
     </div>
   );
