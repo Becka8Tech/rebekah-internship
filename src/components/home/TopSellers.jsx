fa07fb5b8ba5b876a1f1d5a8ce7696f4368b593e
Becka8Tech <rebekahmiller18@gmail.com>
2025-04-24 22:05:04 -0600

    Skeleton Changes

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -42,54 +42,55 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12 wow fadeIn" style={{ visibility: "visible" }}>
+          <div
+            className="col-md-12 wow fadeIn"
+            style={{ visibility: "visible" }}
+          >
             <ol className="author_list">
               {loading ? (
-                new Array(12).fill(0).map((_, index) => (
-                  <li key={index}>
-                    <div className="author_list_pp">
-                      <Skeleton
-                        width="100%"
-                        height="260px"
-                        borderRadius="10px"
-                      />
-                      <div style={{ marginTop: "10px" }}>
+                <div>
+
+                 {new Array(12).fill(0).map((_, index) => (
+                   <div key={index}>
+                      <div className="author_list_pp">
                         <Skeleton
                           width="50px"
                           height="50px"
-                          borderRadius="50%"
-                        />
-                        <Skeleton
-                          width="40%"
-                          height="18px"
-                          borderRadius="5px"
-                          style={{ margin: "0 auto" }}
-                        />
+                          borderRadius="100%"
+                          />
+                        <i className="fa fa-check"></i>
+                          </div>
+                        <div style={{ marginTop: "10px" }}>
+                          <Skeleton
+                            width="40%"
+                            height="18px"
+                            borderRadius="5px"
+                            />
+                        </div>
+                    </div>
+                  ))}
+                  </div>
+               ) : data.map((item) => (
+                    <div key={item.authorId}>
+                      <span>{item.id}.</span>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={item.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${item.authorId}`}>
+                          {item.authorName}
+                        </Link>
+                        <span>{item.price} ETH</span>
                       </div>
                     </div>
-                  </li>
-                ))
-              ) : (
-                data.map((item) => (
-                  <div key={item.authorId}>
-                    <span>{item.id}.</span>
-                    <div className="author_list_pp">
-                      <Link to={`/author/${item.authorId}`}>
-                        <img
-                          className="lazy pp-author"
-                          src={item.authorImage}
-                          alt=""
-                        />
-                        <i className="fa fa-check"></i>
-                      </Link>
-                    </div>
-                    <div className="author_list_info">
-                      <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
-                      <span>{item.price} ETH</span>
-                    </div>
-                  </div>
-                ))
-              )}
+                  ))}
             </ol>
           </div>
         </div>
